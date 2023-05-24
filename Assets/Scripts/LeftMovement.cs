572fbf74294a6b52baeb1c897d0c98f3a5dcb15d
OzgurBoruzanci <ozgurboruzanc@gmail.com>
2023-05-24 21:32:08 +0300

    Added prefabs and movement ground

---
+++ Assets/Scripts/LeftMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftMovement : MonoBehaviour
+{
+    [SerializeField]
+    float _speed;
+    BoxCollider2D _boxCollider;
+    float _groundWidht;
+    void Start()
+    {
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _groundWidht = _boxCollider.size.x;
+    }
+
+    void Update()
+    {
+        MoveX();
+        PositionLoop();
+
+    }
+
+    void MoveX()
+    {
+        transform.position = new Vector2(transform.position.x - _speed*Time.deltaTime, transform.position.y);
+    }
+    void PositionLoop()
+    {
+        if (transform.position.x <= -_groundWidht)
+        {
+            transform.position = new Vector2(transform.position.x + 2 * _groundWidht, transform.position.y);
+        }
+    }
+}
